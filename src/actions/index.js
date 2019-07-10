ff7d358a33a3c5b0d2b13af79abd0cba80350a4d
Ruslan Khomiak <ruslan.khomiak@stfalcon.com>
2019-07-10 23:09:38 +0300

    WIP development components

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+
+const booksLoaded = (newBooks) => {
+  return {
+    type: 'BOOK_LOADED',
+    payload: newBooks,
+  }
+};
+
+export {
+  booksLoaded,
+};
---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { withBookstoreService } from '../hoc';
+
+import './app.css';
+
+const App = ({ bookstoreService }) => {
+  console.log(bookstoreService.getBooks());
+  return <div>App</div>
+};
+
+export default withBookstoreService(App);
