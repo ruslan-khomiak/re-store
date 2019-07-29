f491bdb430d618a8534932a482ecd153a760a413
Ruslan Khomiak <ruslan.khomiak@stfalcon.com>
2019-07-29 22:30:34 +0300

    added loading

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
+import Spinner from '../spinner';
 import { connect } from 'react-redux';
 
 import { withBookstoreService } from '../hoc';
@@ -11,14 +12,17 @@ import './book-list.css';
 class BookList extends Component {
 
   componentDidMount() {
-    const { bookstoreService } = this.props;
-    const data = bookstoreService.getBooks();
-
-    this.props.booksLoaded(data);
+    const { bookstoreService, booksLoaded } = this.props;
+    bookstoreService.getBooks()
+      .then((data) => booksLoaded(data));
   }
 
   render() {
-    const { books } = this.props;
+    const { books, loading } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    }
 
     return(
       <ul className="book-list">
@@ -36,7 +40,7 @@ class BookList extends Component {
   }
 }
 
-const mapStateToProps = ({ books }) => ({ books });
+const mapStateToProps = ({ books, loading }) => ({ books, loading });
 
 const mapDispatchToProps = {
  booksLoaded
