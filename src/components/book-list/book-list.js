6bc8eae085a42f556b5c169a2a250bd7f848ba3f
Ruslan Khomiak <ruslan.khomiak@stfalcon.com>
2019-08-06 15:38:42 +0300

    added redux to shopping-cart-table

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,7 +10,23 @@ import { compose } from '../../utils';
 import './book-list.css';
 import ErrorIndicator from '../error-indicator/error-indicator';
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+ return(
+  <ul className="book-list">
+   {
+    books.map((book) => {
+     return(
+      <li key={ book.id }>
+       <BookListItem book={ book } />
+      </li>
+     )
+    })
+   }
+  </ul>
+ );
+};
+
+class BookListContainer extends Component {
 
   componentDidMount() {
    this.props.fetchBooks();
@@ -27,19 +43,7 @@ class BookList extends Component {
       return <ErrorIndicator />
     }
 
-    return(
-      <ul className="book-list">
-        {
-          books.map((book) => {
-            return(
-              <li key={ book.id }>
-                <BookListItem book={ book } />
-              </li>
-            )
-          })
-        }
-      </ul>
-    );
+    return <BookList books={ books } />
   }
 }
 
@@ -54,4 +58,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 export default compose(
  withBookstoreService(),
  connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
